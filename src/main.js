14570b7c86f3ca1dca90a1e6c8ff2a0642f2baa2
7Yuka7 <shanfengwqwg@163.com>
2023-02-25 18:23:24 +0800

    完成了home页面的所有静态界面的布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 //引入路由
 import router from '@/router'
 
+//全局组件引入区域
+import TypeNav from '@/pages/Home/TypeNav'
+
+//注册全局组件
+Vue.component('TypeNav',TypeNav)
 
 Vue.config.productionTip = false
 
